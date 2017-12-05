336b19fd6adcda5660fa33bd77573289a927755a
Martin Butler <martin.butler@tomologic.com>
2017-12-05 11:28:15 +0100

    Add Day5_2 as a separate object in Day5.scala

--- src/main/scala-2.12/se/apogo/advent17/Day5.scala
+++ src/main/scala-2.12/se/apogo/advent17/Day5.scala
@@ -8,7 +8,7 @@ object Day5_1 extends App {
   def loop(n: Int, inst: IndexedSeq[Int], i: Int): Int = {
     if (!inst.indices.contains(i)) n else {
       val jump = inst(i)
-      loop(n+1, inst.updated(i, if (jump < 3) jump+1 else jump-1), i+jump)
+      loop(n+1, inst.updated(i, jump+1), i+jump)
     }
   }
 
@@ -17,6 +17,20 @@ object Day5_1 extends App {
   println(result)
 }
 
+object Day5_2 extends App {
+  val input: IndexedSeq[Int] = Source.fromResource("se/apogo/advent17/input05.txt").getLines.map(Integer.parseInt).toIndexedSeq
+
+  def loop(n: Int, inst: IndexedSeq[Int], i: Int): Int = {
+    if (!inst.indices.contains(i)) n else {
+      val jump = inst(i)
+      loop(n+1, inst.updated(i, if (jump < 3) jump+1 else jump-1), i+jump)
+    }
+  }
+
+  val result = loop(0, input, 0)
+
+  println(result)
+}
 
 
 
