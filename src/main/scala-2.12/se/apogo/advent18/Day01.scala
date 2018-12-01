611b185aea491613f5da9a88bfc11420cfa85c5d
Martin Butler <martin.butler@tomologic.com>
2018-12-01 07:18:38 +0100

    Add solutions to Day 1 (2018)

---
+++ src/main/scala-2.12/se/apogo/advent18/Day01.scala
@@ -0,0 +1,23 @@
+package se.apogo.advent18
+
+import scala.io.Source
+
+object Day01 extends App {
+  val changes = Source.fromResource("se/apogo/advent18/input01.txt").getLines.map(_.trim).map(Integer.parseInt)
+
+  println(changes.sum)
+}
+
+object Day02 extends App {
+  val changes: Seq[Int] = Source.fromResource("se/apogo/advent18/input01.txt").getLines.map(_.trim).map(Integer.parseInt).toSeq
+
+  def freqs(cs: Seq[Int], f: Int, seen: Set[Int]): Int = {
+    if (cs.isEmpty) freqs(changes, f, seen) else {
+      val newF = f + cs.head
+      if (seen.contains(newF)) newF else
+        freqs(cs.tail, newF, seen + newF)
+    }
+  }
+
+  println(freqs(Nil, 0, Set(0)))
+}
