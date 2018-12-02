6f2c9f2bf40ce6dd3d2ce8bdacee43e2de1a4e02
Martin Butler <martin.butler@tomologic.com>
2018-12-02 06:38:13 +0100

    Add solutions to day 2 2018

---
+++ src/main/scala-2.12/se/apogo/advent18/Day02.scala
@@ -0,0 +1,35 @@
+package se.apogo.advent18
+
+import scala.io.Source
+
+object Day02 extends App {
+  val changes: Seq[String] = Source.fromResource("se/apogo/advent18/input02.txt").getLines.map(_.trim).toSeq
+
+  def count(s: String)(c: Char) = s.count(_ == c)
+  val counts: Seq[IndexedSeq[Int]] = changes.map(s => s.map(count(s)))
+
+  println(counts.count(_.exists(_ == 2)) * counts.count(_.exists(_ == 3)))
+}
+
+object Day02_2 extends App {
+  val changes: Seq[List[Char]] = Source.fromResource("se/apogo/advent18/input02.txt").getLines.map(_.trim.toList).toSeq
+
+  def check(l1: List[Char], l2: List[Char], acc: String, change: Boolean): Option[String] = {
+    (l1, l2) match {
+      case (Nil, _) if change => Some(acc)
+      case (Nil, _) => None
+      case (a::as, b::bs) if !change && a != b => check(as, bs, acc, true)
+      case (a::_, b::_) if a != b => None
+      case (a::as, _::bs) => check(as, bs, acc :+ a, change)
+    }
+  }
+
+  val result =
+    for {
+      i <- 0 to changes.size - 2
+      j <- i + 1 until changes.size
+      letters <- check(changes(i), changes(j), "", false)
+    } yield letters
+
+  println(result.head)
+}
